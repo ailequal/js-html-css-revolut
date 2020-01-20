c309c0dd0245459b905275ae58cc9b2c4635ca55
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-20 16:41:00 +0100

    Working on js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,35 @@
 // main
 $(document).ready(
   function () {
-
+    // $('.accounts').hover(
+    //   function () {
+    //     dropdownHover();
+    //   }
+    // );
+    $('.accounts').click(
+      function () {
+        dropdownClick();
+      }
+    );
+    console.log($('.accounts'));
   }
 );
 
 // function
 // hover on the item and make a dropdown menu appear
-function dropdownHover () {
-
+function dropdownHover() {
+  // var hover = $(this);
+  var hover = $('.accounts');
+  var dropdown = hover.children('.dropdown');
+  dropdown.addClass('active');
 }
 
 // click on the item and make a dropdown menu appear
-function dropdownClick () {
-
+function dropdownClick() {
+  // var hover = $(this);
+  var hover = $('.accounts');
+  console.log(hover);
+  var dropdown = hover.children('.dropdown');
+  console.log(dropdown);
+  dropdown.toggle('active');
 }
