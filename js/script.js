b4afc3e68f705f71c5cdb40e905f8e55a210ea99
ailequal <37016865+ailequal@users.noreply.github.com>
2020-01-20 16:22:41 +0100

    Improved dropdown css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+// main
+$(document).ready(
+  function () {
+
+  }
+);
+
+// function
+// hover on the item and make a dropdown menu appear
+function dropdownHover () {
+
+}
+
+// click on the item and make a dropdown menu appear
+function dropdownClick () {
+
+}
